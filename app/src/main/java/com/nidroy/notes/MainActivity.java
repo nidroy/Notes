1b486a9d8446d7f960577a98eb6e3977e3a4fe6b
nidroy <nidroy@mail.ru>
2022-09-19 13:14:00 +0300

    preparing to work with the database

--- app/src/main/java/com/nidroy/notes/MainActivity.java
+++ app/src/main/java/com/nidroy/notes/MainActivity.java
@@ -3,12 +3,31 @@ package com.nidroy.notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // переменные компонентов верстки
+    Button button;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // определление переменных через компоненты в XML разметки
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        // обработчик нажатия на кнопку
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.setText("Hellow");
+            }
+        });
     }
 }
